7de476a767f235d3f87a652cce563be23cbcc22f
koaudi <k4circles@gmail.com>
2020-01-30 17:13:11 +0300

    more hackerrank sql

--- sql/leet-code-questions.sql
+++ sql/leet-code-questions.sql
@@ -34,3 +34,35 @@ FROM company c left join employee e
                 on c.company_code = e.company_code
 GROUP BY c.company_code, c.founder
 ORDER BY SUBSTR(c.company_code,2,length(c.company_code));
+
+#Manhattan distance
+select round(abs(min(lat_n) - max(lat_n) + min(long_w) - max(long_w)),4) from station;
+
+#Euclidian distance
+select round(sqrt((min(lat_n) - max(lat_n))*(min(lat_n) - max(lat_n)) + ((min(long_w) - max(long_w))*(min(long_w) - max(long_w)))),4) from station;
+
+
+#Median
+select round(sqrt((min(lat_n) - max(lat_n))*(min(lat_n) - max(lat_n)) + ((min(long_w) - max(long_w))*(min(long_w) - max(long_w)))),4) from station;
+
+#select grades between a range
+select case when grade < 8 then null else name end,g.grade,s.marks from students s, grades g where
+s.marks between g.min_mark and g.max_mark order by grade desc,s.name;	
+
+### CREATING LEADER BOARD COUNT WHO HAS MORE THAN 1 HIGH SCORE
+SELECT 
+    h.hacker_id, h.name
+FROM
+    submissions s
+        LEFT JOIN
+    hackers h ON s.hacker_id = h.hacker_id
+        LEFT JOIN
+    challenges c ON s.challenge_id = c.challenge_id
+        LEFT JOIN
+    difficulty d ON d.difficulty_level = c.difficulty_level
+WHERE
+    s.score = d.score
+        AND d.difficulty_level = c.difficulty_level
+GROUP BY h.hacker_id , h.name
+HAVING COUNT(h.hacker_id) > 1
+ORDER BY COUNT(h.hacker_id) DESC , h.hacker_id ASC;
