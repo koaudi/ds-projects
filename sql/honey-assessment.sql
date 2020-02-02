00a7c8984bf9cd51f363dcbfe27e0e294c217407
koaudi <k4circles@gmail.com>
2020-02-02 12:55:10 +0300

    added honey assessments

--- sql/honey-assessment.sql
+++ sql/honey-assessment.sql
@@ -1,8 +1,97 @@
+DROP TEMPORARY TABLE transactions;
+
+CREATE TEMPORARY TABLE transactions (
+    country VARCHAR(255),
+    saleValue INT,
+    commissionPercent DECIMAL(3,2),
+    month VARCHAR(255)
+);
+
+
+
+INSERT INTO 
+   transactions (country,saleValue,commissionPercent,month)
+VALUES
+     ('KENYA',100,0.25,'2019-01'),
+	 ('KENYA',200,0.25,'2019-02'),
+	 ('KENYA',300,0.25,'2019-03'),
+	 ('KENYA',400,0.25,'2019-04'),
+	 ('RWANDA',100,0.25,'2019-01'),
+	 ('RWANDA',100,0.25,'2019-02'),
+	 ('RWANDA',100,0.25,'2019-03'),
+	 ('RWANDA',100,0.25,'2019-04'),
+	 ('UGANDA',100,0.25,'2019-01'),
+	 ('UGANDA',150,0.25,'2019-02'),
+	 ('UGANDA',150,0.25,'2019-03'),
+	 ('UGANDA',150,0.25,'2019-04');
+
+
+
+
 select 
 country,
-month(timestamp) as month,
-sum(saleValue * commissionPercent) as revenue
+month(timestamp),
+sum(saleValue * commissionPercent) as revenue,
+100 * (count(*) - lag(count(*), 1) over (order by month(timestamp))) / lag(count(*), 1) over (order by month(timestamp))) as growth
 from
 transactions
 group by
-country, month
+country, month, 1;
+order by 1
+
+
+
+SELECT
+  country,
+  month,
+  revenue,
+  CAST(100 * sum(revenue) OVER (ORDER BY month) 
+           / sum(revenue) OVER () AS numeric(10, 2)) percentage
+FROM (
+  select 
+	country,
+	month,
+	sum(saleValue * commissionPercent) as revenue
+	from
+	transactions
+	group by
+	country, month;
+) p
+ORDER BY country, month;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select d.*, (revenue - prev_revenue) / (prev_revenue) as percent_difference
+from (select d.*
+             country,
+			 month,
+             sum(saleValue * commissionPercent) as revenue,
+             lag(sum(saleValue * commissionPercent)) over (partition by country order by month) as prev_revenue
+      from transactions
+	  group by
+	  country, month
+     ) d;
+
+
+
+
+select date_trunc('month', timestamp) as date,
+       count(*) as count,
+       100 * (count(*) - lag(count(*), 1) over (order by timestamp)) / lag(count(*), 1) over (order by timestamp)) || '%' as growth
+from events
+where event_name = 'created chart'
+group by 1
+order by 1
